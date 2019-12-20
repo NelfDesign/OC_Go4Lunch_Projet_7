18d8e8c43432a5ee9bb0a6e9d3a40f624501cbef
NelfDesign <fabrice.design85@gmail.com>
2019-12-20 09:27:59 +0100

    add gitignore rule

---
+++ app/src/main/java/fr/nelfdesign/go4lunch/ui/activity/RestaurantDetail.java
@@ -0,0 +1,16 @@
+package fr.nelfdesign.go4lunch.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.nelfdesign.go4lunch.R;
+
+public class RestaurantDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+    }
+}
